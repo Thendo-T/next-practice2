39455c99b0d87c0424b1a4264b7c94d0796fb954
Thendo-T <Thendothovhakale0002@gmail.com>
2022-07-12 13:32:56 +0200

    bug fix

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,12 +1,3 @@
-import { UserProvider } from '@auth0/nextjs-auth0';
-import { handleAuth } from '@auth0/nextjs-auth0';
-
-function MyApp({Component, pageProps}) {
-    return 
-    (<UserProvider>
-        <Component {... pageProps} />
-        </UserProvider>
-    )
-}
+import { handleAuth } from '@auth0/nexjs-auth0';
 
 export default handleAuth();
