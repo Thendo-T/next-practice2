3e2a9cc43eae1bab75ab354463ff0cf8dab798b4
Thendo-T <Thendothovhakale0002@gmail.com>
2022-07-12 09:53:11 +0200

    Update [...auth0].js

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,12 @@
+import { UserProvider } from '@auth0/nextjs-auth0';
 import { handleAuth } from '@auth0/nextjs-auth0';
+
+function MyApp({Component, pageProps}) {
+    return 
+    (<UserProvider>
+        <Component {... pageProps} />
+        </UserProvider>
+    )
+}
 
 export default handleAuth();
